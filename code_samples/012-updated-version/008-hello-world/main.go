557e85e346587046a8a4361ce9e94d0e682fe635
albukhary <lazizbekkahramonov@gmail.com>
2021-06-18 06:02:59 +0500

    Customized the preview sample

--- code_samples/012-updated-version/008-hello-world/main.go
+++ code_samples/012-updated-version/008-hello-world/main.go
@@ -1,3 +1,4 @@
+/*
 package main
 
 import "fmt"
@@ -25,8 +26,33 @@ func foo() {
 func bar() {
 	fmt.Println("and then we exited")
 }
-
+*/
 // control flow:
 // (1) sequence
 // (2) loop; iterative
 // (3) conditional
+
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Assalamu alaykum guys! This a totally awesome course to follow. May Allah make our journey easy! Ameen")
+	foo()
+	fmt.Println("Something more...")
+	for i:=0;i<10;i++{
+		if i%2==0{
+			fmt.Printf("%v is an even number bro., learn math\n",i)
+		}
+	}
+	bar()
+}
+func foo(){
+	fmt.Println("I am in foo() baby... ")
+
+}
+func bar(){
+	fmt.Println("Exiting main()...")
+}
