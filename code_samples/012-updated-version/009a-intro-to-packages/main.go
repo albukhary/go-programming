eba6b078cf321cb931e005cb99d95391e3d0b32d
albukhary <lazizbekkahramonov@gmail.com>
2021-06-18 10:17:22 +0500

    created a new print function which takes variadic parameters, yeah

--- code_samples/012-updated-version/009a-intro-to-packages/main.go
+++ code_samples/012-updated-version/009a-intro-to-packages/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+/*
 func main() {
 	n, _ := fmt.Println("Hello, playground", 42, true)
 	fmt.Println(n)
+}
+*/
+func main(){
+	n, _ := myPrint("Yeah bro I do not need fmt anymore...")
+	myPrint(n," bytes printed")
+}
+func myPrint(a ... interface{})( int, error){
+	return fmt.Println(a)
 }
